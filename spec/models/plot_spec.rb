7527e66fd62eb158f4500727ea1c62e8a9dc66ad
Kerynn Davis <kerynn.davis@gmail.com>
2025-10-29 16:27:27 -0700

    add plant_list method to plot model

--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Plot, type: :model do
     it { should have_many :plot_plants }
     it { should have_many(:plants).through(:plot_plants) }
   end
+
+  describe "plant_list" do
+    it "returns the names of the plants in the plot" do
+      garden = Garden.create!(name: "Frank Garden", organic: true)
+      plot = garden.plots.create!(size: "small", direction: "West")
+      rose = plot.plants.create!(name: "rose", description: "red and lovely", days_to_harvest: 8)
+      lily = plot.plants.create!(name: "lily", description: "yellow maybe", days_to_harvest: 10)
+      basil = plot.plants.create!(name: "basil", description: "tasty and smells great", days_to_harvest: 5)
+      
+      expect(plot.plant_list).to eq([rose.name, lily.name, basil.name])
+    end
+  end
 end

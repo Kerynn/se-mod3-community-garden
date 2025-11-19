8dd8a5f5f5845cda0394db1ac6d6945c59265fec
Kerynn Davis <kerynn.davis@gmail.com>
2025-11-19 11:53:23 -0800

    add test for failing to create plot_plant

--- spec/requests/api/v1/plots/create_plot_plants_spec.rb
+++ spec/requests/api/v1/plots/create_plot_plants_spec.rb
@@ -23,4 +23,24 @@ RSpec.describe 'Plot Plants API' do
     expect(success_response[:success]).to eq("Plant added to plot successfully")
   end
 
+  it 'will send an error message if not able to add a plant to a plot' do
+    garden = Garden.create!(name: "Frank Garden", organic: true)
+    plot = garden.plots.create!(size: "small", direction: "West")
+
+    expect(plot.plants.empty?).to be true
+
+    headers = { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    body = { plant_id: nil, num_seedlings: 4 }
+    post "/api/v1/plots/#{plot.id}/plants", params: body.to_json, headers: headers
+
+    expect(response).to_not be_successful
+    expect(response).to have_http_status(400)
+    expect(plot.plants.empty?).to be true
+    
+    error_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(error_response).to be_a(Hash)
+    expect(error_response).to have_key(:errors)
+    expect(error_response[:errors]).to eq("Plant must exist")
+  end
 end
